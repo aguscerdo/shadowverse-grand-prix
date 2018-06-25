8efdb4c86a6e5c74870c71aa10ce55f99f8862b0
aguscerdo <agus.marinovic@gmail.com>
2018-06-24 22:14:16 -0700

    Added stage 2

--- analysis.py
+++ analysis.py
@@ -3,23 +3,23 @@ from tools import *
 
 
 
-def analyze_horizontal(df):
+def analyze_horizontal(df, n):
     # ---- Single column distributions -----
     rank_count = count_col(df, "Rank")
-    plot_pie(rank_count, "Rank Distribution", "rank_count")
+    plot_pie(rank_count, "Rank Distribution - Stage {}".format(n), "rank_count_s{}".format(n))
     class_count = count_col(df, "Class")
-    plot_pie(class_count, "Class Distribution", "class_count")
+    plot_pie(class_count, "Class Distribution - Stage {}".format(n), "class_count_s{}".format(n))
 
 
     # Rank Statistics
     rank_class = count_col(df, ['Rank', 'Class'])
-    plot_bar(rank_class, "Ranks by Class", "rank_vs_class", groupby="Rank")
+    plot_bar(rank_class, "Ranks by Class - Stage {}".format(n), "rank_vs_class_s{}".format(n), groupby="Rank")
 
     rank_wins = count_col(df, ['Rank', 'Wins'])
-    plot_bar(rank_wins, "Wins per Rank", "rank_vs_win", groupby='Rank')
+    plot_bar(rank_wins, "Wins per Rank - Stage {}".format(n), "rank_vs_win_s{}".format(n), groupby='Rank')
 
     class_wins = count_col(df, ['Class', 'Wins'])
-    plot_bar(class_wins, "Wins per Class", 'class_vs_win', groupby='Class')
+    plot_bar(class_wins, "Wins per Class - Stage {}".format(n), 'class_vs_win_s{}'.format(n), groupby='Class')
 
 
     # Class Statistics
@@ -30,32 +30,32 @@ def analyze_horizontal(df):
 
         # Class Archetypes
         class_archetype = count_col(dfc, ['Class', 'Archetype'])
-        plot_pie(class_archetype, "Archetype Distribution for {} - Users".format(c), "{}_arch_pie".format(c))
+        plot_pie(class_archetype, "Archetype Distribution for {} - Users - Stage {}".format(c, n), "{}_arch_pie_s{}".format(c, n))
 
 
-def analyze_vertical(df):
+def analyze_vertical(df, n):
     classes = ['Shadowcraft', 'Runecraft', 'Forestcraft', 'Swordcraft', 'Dragoncraft', 'Havencraft', 'Portalcraft', 'Bloodcraft']
-
 
     for c in classes:
         dfc = stack_match(df, c)   # Full
 
         # Avg Winrate
         df_mean = mean_col(dfc, ['Class', 'Class_o'], 'Result')
-        plot_bar(df_mean, 'Class vs Class Winrate for {} - All'.format(c), 'class_vs_class_win_{}'.format(c))
+        plot_bar(df_mean, 'Class vs Class Winrate for {} - All - Stage {}'.format(c, n), '{}_vs_class_s{}'.format(c, n))
 
 
         # Archetypes
         class_archetype = count_col(dfc, ['Class', 'Archetype'])
-        plot_pie(class_archetype, "Archetype Distribution for {} - All".format(c), "{}_full_arch_pie".format(c))
+        phead(class_archetype)
+        plot_pie(class_archetype, "Archetype Distribution for {} - All - Stage {}".format(c, n), "{}_full_arch_pie_s{}".format(c, n))
 
 
     # Most popular decks
     popular_decks = most_popular_n(df, 10)
-    plot_bar(popular_decks, "Most used decks - All", 'most_used_all')
+    plot_bar(popular_decks, "Most used decks - All - Stage {}".format(n), 'most_used_all_s{}'.format(n))
 
     # Highest Winrate Decks
     # best_decks = best_decks_n(df, 10)
-    # plot_bar(best_decks, 'Highest Winrate Decks - All', 'best_deck_all')
+    # plot_bar(best_decks, 'Highest Winrate Decks - All - Stage {}'.format(n), 'best_deck_all_s{}'.format(n))
 
 
--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import tools as t
 from analysis import *
 import matplotlib.pyplot as plt
 
-df = t.r_csv("raw/stage1.csv")
-analyze_horizontal(df)
 
+# # Stage 1
+# df1 = t.r_csv("raw/stage1.csv")
+# analyze_horizontal(df1, 1)
+#
+# dfv1 = t.verticalize("vertical/stage1.csv", df1, force=True)
+# analyze_vertical(dfv1, 1)
 
-dfv = t.verticalize("vertical/stage1.csv", df, force=True)
-analyze_vertical(dfv)
+# Stage 2
+df2 = t.r_csv("raw/stage2.csv")
+analyze_horizontal(df2, 2)
+
+dfv2 = t.verticalize("vertical/stage2.csv", df2, force=True)
+analyze_vertical(dfv2, 2)
