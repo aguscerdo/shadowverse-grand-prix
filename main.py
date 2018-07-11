162f6ad9288270ab9d38da2442a8de4801ddead8
aguscerdo <agus.marinovic@gmail.com>
2018-07-11 10:36:57 -0700

    usage

--- main.py
+++ main.py
@@ -24,6 +24,15 @@ def main(folder, stages, force):
         analyze_vertical(dfv2, 2, folder)
 
 if __name__ == '__main__':
+    """
+    Usage: 'python3 main.py FOLDER_NAME [-s {0|1|2}] [-f]
+    
+    - FOLDER_NAME is container where data is: e.g. BOTS_R_1
+    - stage [-s] is which stages to analyze, 0 for all, 1 for stage 1 only, 2 for stage 2 only
+    - force [-f] is a binary to force re-verticalization.
+        - Verticalized data is saved to a file, if not found it will be performed
+    """
+
     parser = argparse.ArgumentParser()
     parser.add_argument('folder', nargs='?')
     parser.add_argument('-f', '--force',action='store_true', default=False)
